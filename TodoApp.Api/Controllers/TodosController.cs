b4dc31fa04217ddbf59dea2d1cb765f22042f4f8
veyselalceylan <veyselalceylan@gmailcom>
2026-05-13 14:45:16 +1000

    feat(backend): add POST endpoint to create todos

--- TodoApp.Api/Controllers/TodosController.cs
+++ TodoApp.Api/Controllers/TodosController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using TodoApp.Api.Data;
+using TodoApp.Api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace TodoApp.Api.Controllers;
 
@@ -12,5 +14,21 @@ public class TodosController : ControllerBase
     public TodosController(AppDbContext context)
     {
         _context = context;
+    }
+
+   [HttpGet]
+    public async Task<ActionResult<IEnumerable<Todo>>> GetTodos()
+    {
+        return await _context.Todos.ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Todo>> CreateTodo(Todo todo)
+    {
+        todo.Id = Guid.NewGuid();
+        _context.Todos.Add(todo);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetTodos), new { id = todo.Id}, todo);
     }
 }
