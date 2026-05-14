9f3f0996d9716b5ddc27c68ced9f4aaab9632449
veyselalceylan <veyselalceylan@gmailcom>
2026-05-14 13:56:49 +1000

    fixed bug of playwright

--- TodoApp.Ui/playwright.config.ts
+++ TodoApp.Ui/playwright.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
     trace: 'on-first-retry',
   },
 
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+
   projects: [
     {
       name: 'chromium',
@@ -15,6 +18,16 @@ export default defineConfig({
     },
   ],
 
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  webServer: [
+    {
+      command: 'dotnet run --project ../TodoApp.Api/TodoApp.Api.csproj --urls http://0.0.0.0:5000',
+      url: 'http://127.0.0.1:5000',
+      timeout: 120000,
+    },
+    {
+      command: 'npm start -- --host 0.0.0.0 --poll=2000',
+      url: 'http://127.0.0.1:4200',
+      timeout: 120000,
+    },
+  ],
 });
