bd0705a8cdd3f16af070332510a08def19e352bf
veyselalceylan <veyselalceylan@gmailcom>
2026-05-14 21:45:58 +1000

    bug fixed

--- TodoApp.Ui/playwright.config.ts
+++ TodoApp.Ui/playwright.config.ts
@@ -4,21 +4,21 @@ export default defineConfig({
   testDir: './tests',
 
   use: {
-    baseURL: 'http://127.0.0.1:4200',
+    baseURL: 'http://localhost:4200',
     trace: 'on-first-retry',
   },
 
   webServer: [
     {
-      command: 'dotnet run --urls=http://0.0.0.0:5000',
-      url: 'http://127.0.0.1:5000',
+      command: 'dotnet run',
+      url: 'http://localhost:5000',
       reuseExistingServer: !process.env.CI,
       cwd: '../TodoApp.Api',
       timeout: 120 * 1000,
     },
     {
-      command: 'npm start -- --host 0.0.0.0',
-      url: 'http://127.0.0.1:4200',
+      command: 'npm start',
+      url: 'http://localhost:4200',
       reuseExistingServer: !process.env.CI,
       cwd: '.',
       timeout: 120 * 1000,
